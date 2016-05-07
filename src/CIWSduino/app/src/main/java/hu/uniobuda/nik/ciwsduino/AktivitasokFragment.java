05d9eca8da2221257d14ac502f9312c75c6e7186
CyberZeroHun <ambrus.attila@stud.uni-obuda.hu>
2016-05-07 22:33:52 +0200

    AsyncTask

--- src/CIWSduino/app/src/main/java/hu/uniobuda/nik/ciwsduino/AktivitasokFragment.java
+++ src/CIWSduino/app/src/main/java/hu/uniobuda/nik/ciwsduino/AktivitasokFragment.java
@@ -1,9 +1,9 @@
 package hu.uniobuda.nik.ciwsduino;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.AppCompatTextView;
 import android.support.v7.widget.ListViewCompat;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,8 +15,6 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Arrays;
@@ -28,6 +26,75 @@ import static hu.uniobuda.nik.ciwsduino.GlobalisKonstansok.*;
  */
 public class AktivitasokFragment extends Fragment {
 
+    private class HatterbenTolt extends AsyncTask<String, Boolean, Boolean> {
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+
+            //mindig ürítjük a listát, mert az új eredményeket írjuk bele
+            aa.ListaTorles();
+            //beolvassuk az URL-ről a JSON-t
+            String jsonFromhtml="";
+            InputStream is = null;
+            try {
+                URL u = new URL(AKTIVITASOK_LINK);
+                URLConnection uc = u.openConnection();
+                is = uc.getInputStream();
+                //beolvassuk a http kérésre adott választ
+                int hossz=0;
+                byte[] buff = new byte[1024];
+                //1024 byte-onként olvassuk amíg van mit
+                while((hossz= is.read(buff))!= -1){
+                    jsonFromhtml=jsonFromhtml.concat(Arrays.toString(buff));
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                return false;
+            } finally {
+                //a végén próbáljuk lezárni a fájlt, ha le lehet
+                try {
+                    if (is != null) {
+                        is.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            //feldolgozzuk a JSON-t Aktivitásokra és feltöltjük vele az adaptert
+            try{
+                //kinyerjük a gyökér elemet
+                JSONObject jo = new JSONObject(jsonFromhtml);
+                //abból kinyerjük az első szinten az összes aktivitás objektumot
+                JSONArray ja = jo.getJSONArray("aktivitas");
+
+                String ido;
+                int szog, tav;
+                //nincs FOREACH a JSON object-re, ezért FOR-t kell használni
+                for (int i=0; i< ja.length();i++) {
+                    JSONObject akt = ja.getJSONObject(i);
+                    //minden objektumnak kinyerjük a tulajdonságait
+                    ido=akt.optString("ido").toString();
+                    szog=Integer.parseInt(akt.optString("szog").toString());
+                    tav=Integer.parseInt(akt.optString("tav").toString());
+                    aa.Hozzaadas(new AktivitasObject(ido, szog, tav));
+                }
+            } catch (JSONException e){
+                e.printStackTrace();
+                return false;
+            }
+
+            return true;
+        }
+
+        //miután kész, frissítsük az UI szálban a ListView-t
+        @Override
+        protected void onPostExecute(Boolean eredmeny) {
+            //ha sikeres volt a letöltés, akkor frissíttetjük csak
+            if(eredmeny) lv.invalidate();
+        }
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -36,63 +103,23 @@ public class AktivitasokFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_aktivitasok, container, false);
     }
 
+    //az adapter, amibe az adatokat töltjük
+    AktivitasAdapter aa;
+    //és a ListView amely megjeleníti
+    ListViewCompat lv;
+
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
         //megkeressük a listát
-        ListViewCompat lv = (ListViewCompat) view.findViewById(R.id.aktivitaslista);
+        lv = (ListViewCompat) view.findViewById(R.id.aktivitaslista);
         //összekötjük a listát az adapterével
-        AktivitasAdapter aa = new AktivitasAdapter();
+        aa = new AktivitasAdapter();
         lv.setAdapter(aa);
 
-        //beolvassuk az URL-ről a JSON-t
-        String jsonFromhtml="";
-        InputStream is = null;
-        try {
-            URL u = new URL(AKTIVITASOK_LINK);
-            URLConnection uc = u.openConnection();
-            is = uc.getInputStream();
-            //beolvassuk a http kérésre adott választ
-            int hossz=0;
-            byte[] buff = new byte[1024];
-            //1024 byte-onként olvassuk amíg van mit
-            while((hossz= is.read(buff))!= -1){
-                jsonFromhtml=jsonFromhtml.concat(Arrays.toString(buff));
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            //a végén próbáljuk lezárni a fájlt, ha le lehet
-            try {
-                if (is != null) {
-                    is.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        //feldolgozzuk a JSON-t Aktivitásokra és feltöltjük vele az adaptert
-        try{
-            //kinyerjük a gyökér elemet
-            JSONObject jo = new JSONObject(jsonFromhtml);
-            //abból kinyerjük az első szinten az összes aktivitás objektumot
-            JSONArray ja = jo.getJSONArray("aktivitas");
-
-            String ido;
-            int szog, tav;
-            //nincs FOREACH a JSON object-re, ezért FOR-t kell használni
-            for (int i=0; i< ja.length();i++) {
-                JSONObject akt = ja.getJSONObject(i);
-                //minden objektumnak kinyerjük a tulajdonságait
-                ido=akt.optString("ido").toString();
-                szog=Integer.parseInt(akt.optString("szog").toString());
-                tav=Integer.parseInt(akt.optString("tav").toString());
-                aa.Hozzaadas(new AktivitasObject(ido, szog, tav));
-            }
-        } catch (JSONException e){
-            e.printStackTrace();
-        }
+        //kezdetben egyszer lehúzzuk az adatokat
+        AsyncTask h = new HatterbenTolt();
+        h.execute();
     }
 }
